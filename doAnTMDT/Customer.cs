02837b975bc7aa9008022de9773b1875a5d80fa9
ltn-2012 <74538393+ltn-2012@users.noreply.github.com>
2021-01-06 21:45:54 +0700

    Add files via upload

---
+++ doAnTMDT/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace doAnTMDT.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int IDCus { get; set; }
+        public int CodeCus { get; set; }
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "bắt buộc !")]
+        public string Email { get; set; }
+        [Display(Name = "Address")]
+        [Required(ErrorMessage = "bắt buộc !")]
+        public string Address { get; set; }
+        [Display(Name = "PhoneNumber")]
+        [Required(ErrorMessage = "bắt buộc !")]
+        public int Phone_Cus { get; set; }
+        public ICollection<Order> Orders { get; set; }
+
+    }
+}
